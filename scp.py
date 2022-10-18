68f61220e25ee0263181a292af26c901a273173e
JesusOS <ortegasanchez.jesusm@gmail.com>
2022-10-18 14:41:00 +0200

    Script para hacer un scp basado en preguntas al usuario -- editado

--- scp.py
+++ scp.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-#scp -i rutakeyprivada ruta origen usuario@ip:/ruta/destino
 from os import system
-
 
 def limpiar():
     system("clear")
@@ -10,45 +8,31 @@ def limpiar():
 
 print("1.Key")
 print("2.passwd")
-#metodo = input("¿Como quieres realizar la conexión?")
 print("¿Como quieres realizar la conexión?")
 metodo = input()
 
-#limpiar()
-
-#origen = input("Introduce la ruta del archivo a copiar")
-#system("clear")
-
 print("Introduce la ruta del archivo a copiar")
 origen = input()
-#limpiar()
 
-#usuario = input("¿Que usuario se va a conectar?")
 print("¿Que usuario se va a conectar?")
 usuario = input()
-#limpiar()
 
-#ip = input("¿Cual es la ip del servidor remoto?")
 print("¿Cual es la ip del servidor remoto?")
 ip = input()
-#limpiar()
 
-#port = input("¿Que puerto desea usar?")
 print("¿Que puerto desea usar?")
 port = input()
-#limpiar()
 
-#destino = input("¿Cual es la ruta de destino?")
 print("¿Cual es la ruta de destino?")
 destino = input()
-#limpiar()
 
 if (metodo=="1"):
-    #key = input("¿Donde tienes tu clave privada?")
     print("¿Donde tienes tu clave privada?")
     key = input()
     system(f"scp -i {key} -p{port} {origen} {usuario}@{ip}:{destino}")
 
 elif (metodo=="2"):
-    password = input("introduce la contraseña")
+    password = "bolson"
     system(f"sshpass -p{password} scp -p{port} {origen} {usuario}@{ip}:{destino}")
+
+#Script terminado
